efbe667686de3e81760806d1520e7f669146b97d
Jinx <56936296+cerenyilmazjinx@users.noreply.github.com>
2025-08-02 17:12:02 +0700

    Add main.go
    Co-authored-by: Hajigur <66867581+hajigur69@users.noreply.github.com>

---
+++ cmd/metaluminarix/main.go
@@ -0,0 +1,23 @@
+// cmd/metaluminarix/main.go
+package main
+
+import (
+	"flag"
+	"log"
+	"os"
+
+	"metaluminarix/internal/metaluminarix"
+)
+
+func main() {
+	verbose := flag.Bool("verbose", false, "Enable verbose logging")
+	input := flag.String("input", "", "Input file path")
+	output := flag.String("output", "", "Output file path")
+	flag.Parse()
+
+	app := metaluminarix.NewApp(*verbose)
+	if err := app.Run(*input, *output); err != nil {
+		log.Printf("Error: %v", err)
+		os.Exit(1)
+	}
+}
